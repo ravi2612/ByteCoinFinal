4f1f3a1958f4e314c0f1c0c06a2ab32d8c689b52
Ravi Navarro <75507215+ravi2612@users.noreply.github.com>
2021-07-15 19:46:37 -0300

    finalProject

---
+++ ByteCoin-iOS13-master/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by raji navarro on 15/07/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
